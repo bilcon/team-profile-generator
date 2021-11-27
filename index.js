e2ba7df9edf8e895fcc16fcfcc4863cda91fdbde
Bill Morris <bilcon@swbell.net>
2021-11-27 07:21:16 -0600

    added require folders

---
+++ index.js
@@ -0,0 +1,11 @@
+// link to page creation
+const generateTeam = require('./src/page-template');
+
+// team profiles
+const Manager = require('./lib/Manager');
+const Engineer = require('./lib/Engineer');
+const Intern = require('./lib/Intern'); 
+
+// node modules 
+const fs = require('fs'); 
+const inquirer = require('inquirer');
